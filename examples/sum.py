2e12c6d8878b85c0f064a42a98d278bba00fcfeb
Lint Action <lint-action@samuelmeuli.com>
2024-07-07 07:30:28 +0000

    Fix code style issues with Black

--- examples/sum.py
+++ examples/sum.py
@@ -1,6 +1,8 @@
-def  calculate_sum( a , b): # Intentional spacing issues
+def calculate_sum(a, b):  # Intentional spacing issues
     """This function  calculates the sum of two numbers."""
-    result=a+b 
-return result # Incorrect indentation
+    result = a + b
+
+
+return result  # Incorrect indentation
 
 print(calculate_sum(10, 5))
--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ code_input = st.text_area("Paste your code here:", height=250)
 
 if st.button("Analyze"):
     if code_input:
-        with st.spinner('Analyzing...'):
+        with st.spinner("Analyzing..."):
             code_handler._input_code = code_input
             response = code_handler.analyze_code()
             data = json.loads(response)
@@ -20,16 +20,16 @@ if st.button("Analyze"):
             try:
                 for items in data["issues"]:
                     with st.expander(f"Line `{items['line']}`"):
-                        st.write(items['message'])
+                        st.write(items["message"])
             except:
                 pass
-            
+
             # JSON output
             st.subheader("Raw API response : ")
             with st.expander(f"Expand"):
                 st.json(response)  # Display the Gemini-generated style guide
-            
+
             time.sleep(5)
-            
+
     else:
         st.warning("Please paste your code to analyze.")
