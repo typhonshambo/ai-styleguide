61705489cc52800b25af2495d94719d524844c36
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2024-07-12 20:32:31 +0530

    implemented fastAPI instead of Flask
    - FastAPI is better in terms of logging and handling

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, Request
 from app import CodeAnalyzer
 import json
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/analyze", methods=["POST"])
-def analyze():
-    data = request.get_json()
+@app.post("/analyze")
+async def analyze(request: Request):
+    data = await request.json()
     code = data["code"]
 
     analyzer = CodeAnalyzer()
     analyzer._input_code = code
     style_guide = analyzer.analyze_code()
-    processed_stule_guide = json.loads(style_guide)
-    issues = {"issues": processed_stule_guide}
+    processed_style_guide = json.loads(style_guide)
+    issues = {"issues": processed_style_guide}
     if style_guide:
-        return jsonify({"style_guide": issues}), 200
+        return {"style_guide": issues}
     else:
-        return jsonify({"error": "Analysis failed"}), 500
+        return {"error": "Analysis failed"}, 400
 
 
 if __name__ == "__main__":
